625d69f4ca71983acb406cd2bdb01be138ffc352
Sean Ryan <seanpryan89@gmail.com>
2020-03-12 22:48:23 -0400

    Added PUT route

--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,48 @@
 // Full Documentation - https://docs.turbo360.co
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Profile = require('../models/Profile')
+const Profile = require("../models/Profile");
 
-router.get('/profiles', (req, res) => {
-	Profile.find()
-	.then(profiles => {
-		res.json({			
-			profiles
-		})
-	})
-	.catch(err => {
-		res.json({
-			confirmation: 'fail',
-			message: err.message
-		})
-	})
-})
+router.get("/profiles", (req, res) => {
+  Profile.find()
+    .then(profiles => {
+      res.json({
+        profiles
+      });
+    })
+    .catch(err => {
+      res.json({
+        confirmation: "fail",
+        message: err.message
+      });
+    });
+});
 
-router.post('/profile', (req, res) => {
-	Profile.create(req.body)
-	.then(profile => {
-		res.json({
-			confirmation: 'success',
-			data: profile
-		})
-	})
-	.catch(err => {
-		res.json({
-			confirmation: 'fail',
-			message: err
-		})
-	})
-})
+router.post("/profile", (req, res) => {
+  Profile.create(req.body)
+    .then(profile => {
+      res.json({
+        confirmation: "success",
+        data: profile
+      });
+    })
+    .catch(err => {
+      res.json({
+        confirmation: "fail",
+        message: err
+      });
+    });
+});
 
-module.exports = router
+router.put("/profile/:id", function(req, res) {
+  var query = { id: req.profile.id };
+  newData = req.newData;
+
+  Profile.findOneAndUpdate(query, newData, function(err, doc) {
+    if (err) return res.send(500, { error: err });
+    return res.send("This profile was successfully updated.");
+  });
+});
+
+module.exports = router;
